b774f78e545232c53fe71d78c1e9e4f55ba1852a
Nuno Henriques <nuno.henriques@izertislabs.pt>
2025-01-02 17:54:47 +0000

    populate a class with data seeding

--- ASPNETCoreWebAPITutorial.GameStore.API/Data/GameStoreContext.cs
+++ ASPNETCoreWebAPITutorial.GameStore.API/Data/GameStoreContext.cs
@@ -3,9 +3,25 @@ using Microsoft.EntityFrameworkCore;
 
 namespace ASPNETCoreWebAPITutorial.GameStore.API.Data;
 
-public class GameStoreContext(DbContextOptions<GameStoreContext> options) : DbContext(options)
+public class GameStoreContext : DbContext
 {
-	public DbSet<Game> Games => Set<Game>();
+    public GameStoreContext(DbContextOptions<GameStoreContext> options) : base(options)
+    {
+    }
 
-	public DbSet<Genre> Genres => Set<Genre>();
+    public DbSet<Game> Games => Set<Game>();
+
+    public DbSet<Genre> Genres => Set<Genre>();
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Genre>().HasData(
+            new { Id = 1, Name = "Fighting" },
+            new { Id = 2, Name = "Roleplaying" },
+            new { Id = 3, Name = "Sports" },
+            new { Id = 4, Name = "Racing" },
+            new { Id = 5, Name = "Kids and Family" }
+        );
+    }
 }
+
---
+++ ASPNETCoreWebAPITutorial.GameStore.API/Data/Migrations/20250102175028_SeedGenres.cs
@@ -0,0 +1,78 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace ASPNETCoreWebAPITutorial.GameStore.API.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class SeedGenres : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "EmptyStringExample1",
+                table: "Genres");
+
+            migrationBuilder.DropColumn(
+                name: "EmptyStringExample2",
+                table: "Genres");
+
+            migrationBuilder.InsertData(
+                table: "Genres",
+                columns: new[] { "Id", "Name" },
+                values: new object[,]
+                {
+                    { 1, "Fighting" },
+                    { 2, "Roleplaying" },
+                    { 3, "Sports" },
+                    { 4, "Racing" },
+                    { 5, "Kids and Family" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Genres",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Genres",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Genres",
+                keyColumn: "Id",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Genres",
+                keyColumn: "Id",
+                keyValue: 4);
+
+            migrationBuilder.DeleteData(
+                table: "Genres",
+                keyColumn: "Id",
+                keyValue: 5);
+
+            migrationBuilder.AddColumn<string>(
+                name: "EmptyStringExample1",
+                table: "Genres",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "EmptyStringExample2",
+                table: "Genres",
+                type: "TEXT",
+                nullable: true);
+        }
+    }
+}
